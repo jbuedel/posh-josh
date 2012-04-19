c5185070630e3dec77c504012c78a7043b58d9f9
Josh Buedel <jbuedel@gmail.com>
2012-04-19 08:23:19 -0500

    Now dynamically looking up the bcomp install location based on the 'bc' alias.

--- register-beyondcompare.ps1
+++ register-beyondcompare.ps1
@@ -1,16 +1,21 @@
 function git-registerbeyondcompare {
-	# TODO: Find this dynamically.  Possibly use (maybe require?) the bc alias.
-	$bcompexe = "c:/program files (x86)/beyond compare 3/bcomp.exe"
+	if(get-alias bc) {
 
-	# git difftool 
-	git config --global diff.tool bc3
-	git config --global difftool.bc3.path $bcompexe
+		# git difftool 
+		git config --global diff.tool bc3
+		(get-alias bc ) | %{ git config --global difftool.bc3.path $_.Definition}
 
-	# git 3 way merge
-	git config --global merge.tool bc3
-	git config --global mergetool.bc3.path $bcompexe
+		# git 3 way merge
+		git config --global merge.tool bc3
+		(get-alias bc ) | %{ git config --global mergetool.bc3.path $_.Definition}
 
-	write-host "Beyond Compare has been registered in git for the diff and difftool commands."
+		write-host "Beyond Compare has been registered in git for the diff and difftool commands."
+	}
+	else {
+		write-host "The alias 'bc' needs to exist, and point to bcomp.exe."
+		write-host "Do that like so: set-alias 'c:\Program Files (x86)\Beyond Compare 3\bcomp.exe'"
+		write-host "Then return this command."
+	}
 }
 
 function hg-registerbeyondcompare {
